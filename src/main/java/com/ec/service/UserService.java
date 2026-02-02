808e43cd8970cd0472c2ea386c2497c4b0ca91e5
yamashita <takahumip7@gmail.com>
2026-02-03 00:29:57 +0900

    Implement user login servie

--- src/main/java/com/ec/service/UserService.java
+++ src/main/java/com/ec/service/UserService.java
@@ -3,6 +3,8 @@ package com.ec.service;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.ec.dto.UserLoginRequest;
+import com.ec.dto.UserLoginResponse;
 import com.ec.dto.UserRegisterRequest;
 import com.ec.dto.UserRegisterResponse;
 import com.ec.entity.User;
@@ -30,7 +32,6 @@ public class UserService {
 		String hashed = passwordEncoder.encode(dto.getPassword());
 		
 		// userのentity作成
-		
 		User user = new User(dto.getName(), dto.getEmail(), hashed);
 		// 保存
 		User savedUser = userRepository.save(user);
@@ -40,5 +41,17 @@ public class UserService {
 	}
 
 	//ログイン
+	public UserLoginResponse login(UserLoginRequest loginDto) {
+		// emailでユーザーを取得
+		User user = userRepository.findByEmail(loginDto.getEmail()).orElseThrow(() -> new IllegalArgumentException("USER_NOT_FOUND"));
+		
+		// パスワードチェック(rawとhashの比較)
+		if (!passwordEncoder.matches(loginDto.getPassword(), user.getPassword())) {
+			throw new IllegalArgumentException("PASSWORD_INCORRECT");
+		}
+		
+		// ログイン成功→レスポンスDTOを返す
+		return new UserLoginResponse(user.getId(), user.getName(),user.getEmail());
+	}
 	//ユーザー情報取得(ID)
 }
